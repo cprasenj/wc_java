8dcec8bd785524282676c93d3f1bf9f29a2ee4bb
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-02-17 14:29:25 +0530

    charCount developed

--- wc.java
+++ wc.java
@@ -1,6 +1,11 @@
 public class wc{
 	private String expression = null;
-	public void wc(String exp) {
+
+	public wc(String exp) {
 		this.expression = exp;
+	}
+
+	public int charCount() {
+		return expression.length();
 	}
 }
--- wcTest.java
+++ wcTest.java
@@ -3,7 +3,18 @@ import static org.junit.Assert.*;
 
 public class wcTest{
 	@Test
-	public void add(){
-		assertEquals(2,2);
+	public void charCount_001() {
+		console.log("counts the number of a characters given string");
+		String text = "hello, this is a good day. What do you think?";
+		wc wC = new wc(text);
+		assertEquals(wC.charCount(),45);
+	}
+
+	@Test
+	public void wordCount_001() {
+		console.log("counts the number of a given string");
+		String text = "hello, this is a good day. What do you think?";
+		wc wC = new wc(text);
+		assertEquals(wC.charCount(),45);
 	}
 }
