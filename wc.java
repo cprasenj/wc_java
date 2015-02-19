12036f395a14ed2a798905c4cea7ea6d5c02c9d1
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2015-02-19 15:21:47 +0530

    wc version 2 without files and edge cases

--- wc.java
+++ wc.java
@@ -1,30 +1,79 @@
 public class wc{
 	private String expression = null;
+	private String options = null,result = "";
+	private boolean w = false,l = false,c = false,wr = false;
+	private int len;
+	private char[] tmp = null;
 
 	public wc(String exp) {
 		this.expression = exp;
 	}
 
-	public int charCount() {
+	public wc(String exp,String options) {
+		this.expression = exp;
+		this.options = options;
+	}
+
+	private int charCount() {
 		return expression.length();
 	}
-	private int occurenceOfChar(char c) {
-		int i,count = 0,limit = this.charCount();
-		char[] tmp = expression.toCharArray();
-		for(i = 0;i<limit;i++) 
-			if(tmp[i] == c)
-				count++;
-		return count;
-	}
-	public int wordCount() {
-		return occurenceOfChar(' ') + 1;
+
+	private int wordCount() {
+		return expression.trim().split("\\W+").length;
 	}
 
-	public int lineCount() {
-		return occurenceOfChar('\n') + 1;
+	private int lineCount() {
+		return expression.split("\r\n|\r|\n").length;
 	}
 
-	public String wcBasicResult() {
-		return Integer.toString(lineCount())+ " " +Integer.toString(wordCount())+ " " +Integer.toString(charCount());
+	private void giveAproval() {
+		if(expression.matches("[^wlc]+")) {
+			wr = true;
+			w = c = l = false;
+		}
+	}
+
+	private void findOption(char o) {
+	if(o == 'w')
+		w = true;
+	if(o == 'l')
+		l = true;
+	if(o == 'c')
+		c = true;
+	}
+
+	private void optionCreator() {
+		for(int i = 0;i < len;i++) {
+			if(tmp[i] == '-' && i+1 < len)
+				findOption(tmp[i+1]);				
+		} 
+		giveAproval();
+	}
+
+	private void resultAsPerOption() {
+		if(w == true) result += wordCount() + " ";
+		if(l == true) result += lineCount() + " ";
+		if(c == true) result += charCount() + " ";
+	}
+
+	private void giveResultAccordingToOption() {
+		giveAproval();
+		optionCreator();
+		resultAsPerOption();
+	}
+
+	public String wcOutput() {
+		if(options == null)
+			result = wcBasicResult();
+		else{
+			tmp = options.toCharArray();
+			len = options.length();
+			giveResultAccordingToOption();
+		}
+		return result.trim();
+	}
+
+	private String wcBasicResult() {
+		return lineCount() + " " + wordCount() + " " + charCount();
 	}	
 }
--- wcTest.java
+++ wcTest.java
@@ -3,34 +3,34 @@ import static org.junit.Assert.*;
 
 public class wcTest{
 	@Test
-	public void charCount_001() {
-		console.log("counts the number of a characters given string");
-		String text = "hello, this is a good day. What do you think?";
+	public void wcBasicResult_001() {
+		console.log("Gives 2 10 48 for the given string");
+		String text = "hello, this is a good day.\r\nWhat do you think?\r\n",result = "2 10 48";
 		wc wC = new wc(text);
-		assertEquals(wC.charCount(),45);
+		assertEquals(wC.wcOutput(),result);
 	}
 
 	@Test
 	public void wordCount_001() {
-		console.log("counts the number of a words in the given string");
-		String text = "hello, this is a good day. What do you think?";
-		wc wC = new wc(text);
-		assertEquals(wC.wordCount(),10);
+		console.log("Gives 10 for the given string");
+		String text = "hello, this is a good day.\n What do you think?",result = "10";
+		wc wC = new wc(text,"-w");
+		assertEquals(wC.wcOutput(),result);
 	}
 
 	@Test
-	public void lineCount_001() {
-		console.log("counts the number of a lines in the given string");
-		String text = "hello, this is a good day.\n What do you think?";
-		wc wC = new wc(text);
-		assertEquals(wC.lineCount(),2);
+	public void wordCount_002() {
+		console.log("Gives 2 for the given string");
+		String text = "hello, this is a good day.\n What do you think?",result = "2";
+		wc wC = new wc(text,"-l");
+		assertEquals(wC.wcOutput(),result);
 	}
 
 	@Test
-	public void wcBasicResult_001() {
-		console.log("Gives 2 10 47 for the given string");
-		String text = "hello, this is a good day.\n What do you think?",result = "2 10 46";
-		wc wC = new wc(text);
-		assertEquals(wC.wcBasicResult(),result);
+	public void wordCount_003() {
+		console.log("Gives 48 for the given string");
+		String text = "hello, this is a good day.\r\nWhat do you think?\r\n",result = "48";
+		wc wC = new wc(text,"-c");
+		assertEquals(wC.wcOutput(),result);
 	}
 }
